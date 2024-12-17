2ec2180d19c536999abec4edf108e800c64e6c36
Amit kishor <amitkishor@live.com>
2024-12-17 19:44:35 +0530

    add new  file CurrencyController  and ADD 2 NEW API GetCurrencybyiD,GetallCurrency

---
+++ DbOperationWithEFcore_Curd/Controllers/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DbOperationWithEFcore_Curd.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationWithEFcore_Curd.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext appContext;
+        public CurrencyController(AppDbContext appContext)
+        {
+           this.appContext = appContext;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> GetallCurrency()
+        {
+            var result = await appContext.CurrencyTypes.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetCurrencybyiD(int Id)
+        {
+            var result = await appContext.CurrencyTypes.FindAsync(Id);
+            return Ok(result);
+        }
+    }
+}
