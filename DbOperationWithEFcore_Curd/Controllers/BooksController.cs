623f2005242a183b765cfb2bc6231c036c76d3c3
Amit kishor <amitkishor@live.com>
2024-12-18 20:52:46 +0530

    add new book api page

---
+++ DbOperationWithEFcore_Curd/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using DbOperationWithEFcore_Curd.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbOperationWithEFcore_Curd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController(AppDbContext appContext) : ControllerBase
+    {
+        public async Task<IActionResult> AddNewBook([FromForm] Book book)
+        {
+             
+            appContext.Books.Add(book);
+            await appContext.SaveChangesAsync();
+            return Ok(book);
+        }
+    }
+}
--- DbOperationWithEFcore_Curd/obj/Debug/net9.0/DbOperationWithEFcore_Curd.AssemblyInfo.cs
+++ DbOperationWithEFcore_Curd/obj/Debug/net9.0/DbOperationWithEFcore_Curd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbOperationWithEFcore_Curd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dad65d3ee1ab79ee7c8726700b07d637ca2bdee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f95f2fc1fa4007d3093439ddbba1b307428f009")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbOperationWithEFcore_Curd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbOperationWithEFcore_Curd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
